11f5975c48fc3c6fc6b9942b91f5ae8f29218af9
Georgiy Zhurahivskiy <georgiy.zhurahivskiy@elfo.net>
2019-12-03 15:59:56 +0200

    les-7: forms-validity

--- vuetify-dashboard/src/views/Signup.vue
+++ vuetify-dashboard/src/views/Signup.vue
@@ -3,21 +3,22 @@
     <v-row>
       <v-col>
         <h1>Signup</h1>
-        <v-form>
-          <v-text-field label="Email" type="email"></v-text-field>
-          <v-autocomplete
-            label="Which browser do you use?"
-            :items="browsers"
-          ></v-autocomplete>
+        <v-form ref="signUpForm" v-model="formValidity">
+          <v-text-field label="Email" type="email" v-model="email" :rules="emailRules" required></v-text-field>
+          <v-autocomplete label="Which browser do you use?" :items="browsers"></v-autocomplete>
           <v-file-input label="Attach profile picture"></v-file-input>
-          <v-text-field
-            v-model="birthday"
-            label="Birthday"
-            readonly
-          ></v-text-field>
+          <v-text-field v-model="birthday" label="Birthday" readonly></v-text-field>
           <v-date-picker v-model="birthday"></v-date-picker>
-          <v-checkbox label="Agree to terms & conditions"></v-checkbox>
-          <v-btn type="submit" color="primary">Submit</v-btn>
+          <v-checkbox
+            label="Agree to terms & conditions"
+            v-model="aggreToTerms"
+            :rules="aggreToTermsRules"
+            required
+          ></v-checkbox>
+          <v-btn class="mr-4" color="success" @click="validateForm">Validate form</v-btn>
+          <v-btn class="mr-4" :disabled="!formValidity" type="submit" color="primary">Submit</v-btn>
+          <v-btn class="mr-4" color="warning" @click="resetValidation">Reset validation</v-btn>
+          <v-btn color="error" @click="reset">Reset</v-btn>
         </v-form>
       </v-col>
     </v-row>
@@ -27,8 +28,39 @@
 <script>
 export default {
   data: () => ({
-    birthday: '',
-    browsers: ['Chrome', 'Firefox', 'Safari', 'Edge', 'Brave']
-  })
-}
+    formValidity: false,
+    birthday: "",
+    email: "",
+    browsers: ["Chrome", "Firefox", "Safari", "Edge", "Brave"],
+    aggreToTerms: false,
+    aggreToTermsRules: [
+      value =>
+        !!value ||
+        "You must agree to the terms and conditions to sign up for an acconunt."
+    ],
+    emailRules: [
+      value => !!value || "Email is required.",
+      value => value.indexOf("@") !== 0 || "Email should have a username.",
+      value =>
+        value.includes("@") || "Email should should include and @ symbol.",
+      value =>
+        value.indexOf(".") - value.indexOf("@") > 1 ||
+        "Email should contain a valid domain.",
+      value =>
+        value.indexOf(".") <= value.length - 3 ||
+        "Email should contain a valid domain extension."
+    ]
+  }),
+  methods: {
+    resetValidation() {
+      this.$refs.signUpForm.resetValidation();
+    },
+    reset() {
+      this.$refs.signUpForm.reset();
+    },
+    validateForm() {
+      this.$refs.signUpForm.validate();
+    }
+  }
+};
 </script>
